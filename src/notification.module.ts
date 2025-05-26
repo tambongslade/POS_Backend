0824012b9a16915c9c7a63be23a53bac4bf58349
tambong <80495675+tambongslade@users.noreply.github.com>
2025-05-26 06:50:07 +0100

    fix bug

--- src/notification.module.ts
+++ src/notification.module.ts
@@ -1,9 +1,13 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { ProductModule } from './product.module';
 
 @Module({
-  imports: [forwardRef(() => WhatsappModule)], // Use forwardRef to break circular dependency
+  imports: [
+    forwardRef(() => WhatsappModule), // Use forwardRef to break circular dependency
+    ProductModule,
+  ],
   providers: [NotificationService],
   exports: [NotificationService], // Export if other modules need to inject it
 })
