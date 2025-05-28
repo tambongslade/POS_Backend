c0a5008f7451bfb0180ce12a1aae15436ff87a02
tambong <80495675+tambongslade@users.noreply.github.com>
2025-05-28 19:13:42 +0100

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',  // Allow all origins
+    origin: 'https://pos-dashboard-blue.vercel.app',  // Allow 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
     allowedHeaders: ['*'],  // Allow all headers
     exposedHeaders: ['*'],  // Expose all headers
